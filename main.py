9b29b68f7a2cc1d1d1cd387a6bd6c59cf3f78f84
MarzooqMoftah <>
2021-11-06 04:57:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+value = db["key"]
+import random
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+d
+  print("Tails")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
